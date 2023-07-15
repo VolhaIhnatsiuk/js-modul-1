ad2c591ee92539774fe4f32996aa0551103daad1
przystanwrozmyslaniu <iov220291@gmail.com>
2023-07-15 12:08:44 +0200

    practice 3

---
+++ modul-3-5.js
@@ -0,0 +1,139 @@
+// Task 1. Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
+
+// function makeProduct(name, price, callback) {
+//   const product = {
+//     name,
+//     price,
+//     id: Math.random(),
+//   };
+//   callback(product);
+//   //   console.log(callback(product));
+// }
+
+// function showProduct(product) {
+//   console.log(product);
+// }
+
+// makeProduct("iphone", 500, showProduct);
+// makeProduct("dfs", 324, showProduct);
+
+// Task 2. //Напишіть функцію each(array, callback), яка
+//першим параметром приймає масив, а другим - функцію,
+//Яка застосовується до кожного елементу масиву.
+//Функція each повинна повернути новий масив, елементами
+//якого будуть результати виклику callback
+//callback функції повинна множити елементи на 2
+
+// const array = [3, 5, 6, 34, 8, 83, 12, 34];
+// function each(array, callback) {
+//     return array.map(callback)
+// }
+// function mult(num, index, array) {
+//     console.log(index)
+//     console.log(array)
+//     return num*2
+// }
+// console.log (each (array, mult))
+
+// Task 3. 
+// const vehicles = [
+//   {
+//     make: "Honda",
+//     model: "CR-V",
+//     type: "suv",
+//     amount: 14,
+//     price: 24045,
+//     onSale: true,
+//   },
+//   {
+//     make: "Honda",
+//     model: "Accord",
+//     type: "sedan",
+//     amount: 2,
+//     price: 22455,
+//     onSale: true,
+//   },
+//   {
+//     make: "Mazda",
+//     model: "Mazda 6",
+//     type: "sedan",
+//     amount: 8,
+//     price: 24195,
+//     onSale: false,
+//   },
+//   {
+//     make: "Mazda",
+//     model: "CX-9",
+//     type: "suv",
+//     amount: 7,
+//     price: 31520,
+//     onSale: true,
+//   },
+//   {
+//     make: "Toyota",
+//     model: "4Runner",
+//     type: "suv",
+//     amount: 19,
+//     price: 34210,
+//     onSale: false,
+//   },
+//   {
+//     make: "Toyota",
+//     model: "Sequoia",
+//     type: "suv",
+//     amount: 16,
+//     price: 45560,
+//     onSale: false,
+//   },
+// {
+//     make: "Toyota",
+//     model: "Tacoma",
+//     type: "truck",
+//     amount: 4,
+//     price: 24320,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "F-150",
+//     type: "truck",
+//     amount: 11,
+//     price: 27110,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "Fusion",
+//     type: "sedan",
+//     amount: 13,
+//     price: 22120,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "Explorer",
+//     type: "suv",
+//     amount: 6,
+//     price: 31660,
+//     onSale: false,
+//   },
+// ];
+
+// function getModels(cars) {
+//     return cars.map(({ model}) => model)
+    
+// }
+// getModels(vehicles)
+
+// function getTotalPriceOnSale(cars) {
+//     return cars.filter(({ onSale }) => onSale).reduce((acc, { amount, price}) => price * amount + acc, 0)
+    
+// }
+// getTotalPriceOnSale(vehicles)
